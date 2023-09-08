a700c530f1c942dcd13654e666d0d0f20d16e703
Cesar Escudero <cedaesca@gmail.com>
2023-09-08 15:24:58 -0400

    feat: install puppeteer

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ScrapperModule } from './scrapper/scrapper.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ScrapperModule],
   controllers: [AppController],
   providers: [AppService],
 })
